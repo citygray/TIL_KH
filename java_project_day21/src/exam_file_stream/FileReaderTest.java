9f0f402eb73a404ac6295a7d554816808982a784
nanee <nanee9244@gmail.com>
2023-05-22 11:22:38 +0900

    io입출력 - FileReader,FileWriter 예제

--- java_project_day21/src/exam_file_stream/FileReaderTest.java
+++ java_project_day21/src/exam_file_stream/FileReaderTest.java
@@ -1,5 +1,11 @@
 package exam_file_stream;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
 public class FileReaderTest {
 	public static void main(String[] args) {
 		/*
@@ -8,6 +14,33 @@ public class FileReaderTest {
 		 * 이 파일의 내용을 읽어 -> 문자입력 스트림
 		 * 콘솔에 출력
 		 */
+		Writer fw = null;
+		BufferedReader in = null;
+		try {
+			fw = new FileWriter("test.txt");
+			fw.write("가장 위대한 영관은 한 번도 실패하지 않음이 아니라 실패할 때 마다 다시 일어서는데에 있다.");
+			fw.flush();
+			
+			in = new  BufferedReader(new FileReader("test.txt"));
+			
+			String line;
+			while ((line = in.readLine()) != null) {
+				System.out.println(line);
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally{
+			try {
+				if(fw != null) fw.close();
+				if(in != null) in.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} 
+		
 	}
 
 }
