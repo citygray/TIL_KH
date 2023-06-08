453d37616d3f1bb3d681a999af4c69dcfcf6ee44
nanee <nanee9244@gmail.com>
2023-06-08 17:46:36 +0900

    [18일차] collection 예제 요구사항4 - 작성중

---
+++ java_project_day18/src/exam/CapitalGame.java
@@ -0,0 +1,14 @@
+package exam;
+
+/*[요구사항 4]
+ * 나라와 수도 맞추기 게임의 실행
+ */
+
+public class CapitalGame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ java_project_day18/src/exam/Nation.java
@@ -0,0 +1,10 @@
+package exam;
+
+/*[요구사항 4]
+ * 나라 이름(country)과 수도(capital)로 구성된 Nation 클래스를 작성하고 ArrayList 컬렉션을 이용하여 프로그램을 작성하라. 
+ * 초기값으로 9개의 나라이름과 수도를 저장한다.
+ */
+
+public class Nation {
+
+}
