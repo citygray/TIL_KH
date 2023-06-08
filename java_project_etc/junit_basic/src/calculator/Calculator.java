d2d038e68c3ab66e7fac04ca53f44ad398d4ffaf
citygray <https://github.com/citygray>
2023-06-09 07:02:21 +0900

    [ junit] basic

---
+++ java_project_etc/junit_basic/src/calculator/Calculator.java
@@ -0,0 +1,17 @@
+package calculator;
+
+public class Calculator {
+
+	int add(int i,int j){
+		return i+j;
+	}
+	int subtrack(int i,int j){
+		return i-j;
+	}
+	int multiply(int i,int j){
+		return i*j;
+	}
+	int divide(int i,int j){
+		return i/j;
+	}
+}
